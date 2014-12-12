11bfa507dc789356cb5dc5d43e33eb9b520269ee
glyfina <glyfina.f@gmail.com>
2014-12-13 01:17:39 +0530

    Create corr.hh

---
+++ corr.hh
@@ -0,0 +1,63 @@
+
+
+#ifndef __CPU_PRED_CORR_PRED_HH__
+#define __CPU_PRED_CORR_PRED_HH__
+
+#include <vector>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include "base/types.hh"
+#include "cpu/pred/bpred_unit.hh"
+#include "cpu/pred/sat_counter.hh"
+#include "cpu/pred/file.hh"
+using namespace std;
+
+class corrBP : public BPredUnit
+{
+
+public:
+
+corrBP(const Params *params );    // constructor
+
+~corrBP(void);
+
+void uncondBranch(void * &bp_history,Addr branch_addr);
+
+
+bool lookup(Addr branch_addr, void * &bp_history);/* looks up the branch address in the bp_history table*/
+
+void btbUpdate(Addr branch_addr, void * &bp_history);
+
+void update(Addr branch_addr, bool taken, void *bp_history, bool squashed); /* updates the branch address as taken , not taken or squashed in the bp_history table*/
+
+
+ void squash(void *bp_history);
+   
+
+
+void reset();
+
+
+
+private:
+
+
+ void updateGlobalHistTaken(unsigned branch_lower_order);
+
+ void updateGlobalHistNotTaken(unsigned branch_lower_order);
+
+     
+unsigned historytable[16];
+unsigned countertable[16];
+
+uint8_t index;
+uint8_t count;
+unsigned branch_lower_order;
+
+char msg[1000];
+
+file fl;
+};
+
+#endif // __CPU_PRED_CORR_PRED_HH__
